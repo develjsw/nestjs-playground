00b39f87f8d0cd563579a7d79113e1c92105a533
jsw <develjsw@gmail.com>
2026-01-10 10:26:17 +0900

    chore(init): 프로젝트 초기화

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LocationModule } from './location/location.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      cache: true,
-      envFilePath: [`.env.${process.env.NODE_ENV || 'local'}`, '.env'],
-    }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: false,
-        logging: true,
-      }),
-    }),
-    LocationModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
